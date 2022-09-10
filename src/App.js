d754b956174f77704fced06f0611bf08b10d743b
Anna-Mamedova <mamedi289@gmail.com>
2022-09-10 15:57:19 +0300

    add react

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+
+export default function App() {
+  return (
+    <div className="App">
+      <form>
+        <input type = "text"/>
+        <input type = "submit" value = "Search"/>
+      </form>
+    </div>
+  );
+}
